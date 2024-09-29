f4139e6b8397c66cddbe4ed23a9f1dab52bb0438
Nav Faz <naveed.fazlan@gmail.com>
2024-09-29 21:01:27 +0800

    Convert this project from a console project to a WPF project.
    Update .gitignore.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Mass_Image_Merging_And_Renaming_Tool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+A tool that allows operations (i.e. merging, renaming) to be performed on 1 or more images quickly and conveniently.
